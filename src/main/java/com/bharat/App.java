1b81dc81f33b2ab4a6f3123b9f89737f3e693e56
Bharat <Bharat@ikcontech.com>
2023-08-04 13:00:20 +0530

    initial-commit

---
+++ src/main/java/com/bharat/App.java
@@ -0,0 +1,26 @@
+package com.bharat;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+class Container<T extends Number>{
+	T value;
+	public void show() {
+		System.out.println(" the show method is executed");
+	}
+}
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+       
+    	
+    }
+}
